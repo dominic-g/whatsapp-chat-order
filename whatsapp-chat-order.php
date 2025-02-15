7383d3482673afa2ee27762bb76466ac6b75533e
Dominic_G <dominicnjoroge1@gmail.com>
2025-02-15 05:43:34 -0500

    included fontawesome libraries to have the icons to use

--- whatsapp-chat-order.php
+++ whatsapp-chat-order.php
@@ -52,8 +52,8 @@ require_once plugin_dir_path(__FILE__) . 'includes/FloatingIconWhatsAppSettings.
 
 add_filter( 'plugin_action_links_' . plugin_basename(__FILE__), 'apd_settings_link' );
 function apd_settings_link( array $links ) {
-    // $url = get_admin_url() . "options-general.php?page=my-plugin";
-    $settings_link = '<a href="'.get_admin_url ().'options-general.php?page=floating-whatsapp-settings">' . __('Settings', 'wco_whatsappchatorder') . '</a>';;
-      $links[] = $settings_link;
+    $settings_link = '<a href="'.get_admin_url ().'options-general.php?page=floating-whatsapp-settings">' . __('Settings', 'wco_whatsappchatorder') . '</a>';
+
+    array_unshift($links, $settings_link);
     return $links;
 }
